2f777329b1f1d536df8405ca33fc07088fc3c5ef
Kriti Pare <kriti_pare@epam.com>
2024-02-24 12:45:29 +0530

    add movie service functions

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
 const options = {
-  method: 'GET',
   headers: {
     accept: 'application/json',
     Authorization:
@@ -18,5 +17,41 @@ export class MovieService {
 
   getMovies() {
     return this.http.get('https://api.themoviedb.org/3/discover/movie', options)
+  }
+
+  getTvShows() {
+    return this.http.get('https://api.themoviedb.org/3/discover/tv', options)
+  }
+
+  getRatedMovies() {
+    return this.http.get('https://api.themoviedb.org/3/guest_session/guest_session_id/rated/movies', options)
+  }
+
+  getBannerImage(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/images`, options)
+  }
+
+  getBannerVideo(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos`, options);
+  }
+
+  getBannerDetail(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`, options);
+  }
+
+  getNowPlayingMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/now_playing', options)
+  }
+
+  getPopularMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/popular', options)
+  }
+
+  getTopRated() {
+    return this.http.get('https://api.themoviedb.org/3/movie/top_rated', options)
+  }
+
+  getUpcomingMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming', options)
   }
 }
