288eec70239cb65a39100b9dc29fd30fa44edd5e
Kriti Pare <kriti_pare@epam.com>
2024-02-25 15:30:58 +0530

    add dynamic username and signout

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,8 @@ import { Component, Input } from '@angular/core';
 })
 export class HeaderComponent {
   @Input({ required: true }) userImg: string = '';
+  @Input({ required: true }) userName: string = '';
+  auth = inject(AuthService);
   navList = [
     'Home',
     'TV Shows',
